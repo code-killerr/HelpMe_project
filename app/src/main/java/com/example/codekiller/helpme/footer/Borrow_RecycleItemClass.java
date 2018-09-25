94fdbc30bd5e600c19618b29e2bd5b5de6073442
code-killer <txq0917@126.com>
2018-09-25 14:08:37 +0800

    change BorrowEachOtheritem and layout

--- app/src/main/java/com/example/codekiller/helpme/footer/Borrow_RecycleItemClass.java
+++ app/src/main/java/com/example/codekiller/helpme/footer/Borrow_RecycleItemClass.java
@@ -1,4 +1,30 @@
 package com.example.codekiller.helpme.footer;
 
 public class Borrow_RecycleItemClass {
+    private String title;
+    private String introduce;
+    private int[] image;
+    public Borrow_RecycleItemClass(String title,String introduce,int[] image){
+        this.title = title;
+        this.introduce = introduce;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int[] getImage() {
+        return image;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void AllItem(String title,String introduce,int[] image) {
+        this.image = image;
+        this.introduce = introduce;
+        this.title = title;
+    }
 }
--- app/src/main/java/com/example/codekiller/helpme/footer/ShowBorrow.java
+++ app/src/main/java/com/example/codekiller/helpme/footer/ShowBorrow.java
@@ -3,6 +3,7 @@ package com.example.codekiller.helpme.footer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,12 @@ public class ShowBorrow extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.showborrowthings_layout,container,false);
         RecyclerView recyclerView = view.findViewById(R.id.showBorrow_Recycle);
+
+        SearchView topSearch = view.findViewById(R.id.TopSearch);
+        topSearch.setSubmitButtonEnabled(true);//是否添加搜索按鈕
+        topSearch.setIconifiedByDefault(true);
+        //topSearch.onActionViewExpanded();//是否展开搜索框
+        topSearch.setBackgroundColor(0xFFFFFFFF);
     return view;
     }
 }
