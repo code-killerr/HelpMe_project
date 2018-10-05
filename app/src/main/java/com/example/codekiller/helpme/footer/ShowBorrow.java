d90e7e491b4e0d80ad8dae6ed4cf6159e7793b68
code-killer <txq0917@126.com>
2018-10-05 12:47:58 +0800

    change BorrowEachOtheritem and layout

--- app/src/main/java/com/example/codekiller/helpme/footer/ShowBorrow.java
+++ app/src/main/java/com/example/codekiller/helpme/footer/ShowBorrow.java
@@ -2,6 +2,7 @@ package com.example.codekiller.helpme.footer;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
@@ -10,20 +11,31 @@ import android.view.ViewGroup;
 
 import com.example.codekiller.helpme.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShowBorrow extends Fragment{
+    private List<Borrow_RecycleItemClass>borrowList = new ArrayList<>();
     public ShowBorrow(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.showborrowthings_layout,container,false);
+        //设置recycleview
         RecyclerView recyclerView = view.findViewById(R.id.showBorrow_Recycle);
-
-        SearchView topSearch = view.findViewById(R.id.TopSearch);
-        topSearch.setSubmitButtonEnabled(true);//是否添加搜索按鈕
-        topSearch.setIconifiedByDefault(true);
-        //topSearch.onActionViewExpanded();//是否展开搜索框
-        topSearch.setBackgroundColor(0xFFFFFFFF);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        ShowBorrowAdapter adapter = new ShowBorrowAdapter(getActivity(),borrowList);
+        recyclerView.setAdapter(adapter);
 
 
-    return view;
+        return view;
     }
+
+    private void initMember() {//初始化导入数据
+        for (int i = 0; i < 10; i++) {
+            Borrow_RecycleItemClass data = new Borrow_RecycleItemClass("hello", "this is something in here", R.drawable.a1);
+            borrowList.add(data);
+        }
+    }
+
 }
