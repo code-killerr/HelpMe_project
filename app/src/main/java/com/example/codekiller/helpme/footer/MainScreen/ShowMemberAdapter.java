bbf54e96922de8c592da14c1ffc75b73260b07d4
code-killer <txq0917@126.com>
2018-11-04 18:13:21 +0800

    add recyclerview click

--- app/src/main/java/com/example/codekiller/helpme/footer/MainScreen/ShowMemberAdapter.java
+++ app/src/main/java/com/example/codekiller/helpme/footer/MainScreen/ShowMemberAdapter.java
@@ -9,11 +9,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.codekiller.helpme.Index;
+import com.example.codekiller.helpme.Main_Screen;
 import com.example.codekiller.helpme.R;
 
 import java.util.List;
 
-public class ShowMemberAdapter extends RecyclerView.Adapter<ShowMemberAdapter.RecycleHolder> {
+public class ShowMemberAdapter extends RecyclerView.Adapter<ShowMemberAdapter.RecycleHolder>{
     private Context context;
     private List<Main_RecycleItemClass> mMember = null;
     public ShowMemberAdapter(Context context, List<Main_RecycleItemClass> list){
@@ -25,6 +27,7 @@ public class ShowMemberAdapter extends RecyclerView.Adapter<ShowMemberAdapter.Re
         notifyDataSetChanged();//强制调用getView来刷新每个Item的内容。
     }
     static  class RecycleHolder extends RecyclerView.ViewHolder{
+        View ShowMemberView;
         TextView textView;
         ImageView imageView;
         private RecycleHolder(View view){
@@ -36,7 +39,15 @@ public class ShowMemberAdapter extends RecyclerView.Adapter<ShowMemberAdapter.Re
     @Override
     public RecycleHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.showmember_recycle, parent, false);
-        RecycleHolder holder = new RecycleHolder(view);
+        final RecycleHolder holder = new RecycleHolder(view);
+        holder.ShowMemberView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int positon = holder.getAdapterPosition();
+                Main_RecycleItemClass member = mMember.get(positon);
+
+            }
+        });
         return holder;
     }//调用相关布局
 
