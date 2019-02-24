11c090f1dd294d56e9a97b83aae68b00cb9d42fd
code-killer <txq0917@126.com>
2019-02-24 20:24:35 +0800

    Merge branches 'CodeKiller_Code' and 'master' of https://github.com/code-killerr/HelpMe_project into CodeKiller_Code Add backup button and change the actionBar style

---
+++ app/src/main/java/com/example/codekiller/helpme/Main_Screen_Subordinates_Project/MemberInfo.java
@@ -0,0 +1,28 @@
+package com.example.codekiller.helpme.Main_Screen_Subordinates_Project;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.codekiller.helpme.R;
+
+public class MemberInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_info);
+        ActionBar actionBar = this.getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
